1632c45b78754751e34d2d109b284c08991338ec
Joe Graham <jmg3484@rit.edu>
2017-06-15 16:16:58 -0400

    implement db_connect

---
+++ db.php
@@ -0,0 +1,12 @@
+<?php
+function db_connect() {
+
+    $db = mysqli_connect('localhost', 'root', 't', 'portal');
+
+    if (mysqli_connect_errno($db) > 0) {
+        die('Unable to connect to database: [' . 
+        mysqli_connect_error($db) .']');
+    }
+
+return $db;
+?>
