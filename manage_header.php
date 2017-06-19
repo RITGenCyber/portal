a08a666f2507bd1bc35baa80a6eb66ba953a4ec4
Joe Graham <jmg3484@rit.edu>
2017-06-19 13:05:14 -0400

    spacing

--- manage_header.php
+++ manage_header.php
@@ -1,11 +1,11 @@
 <?php
-    $managementLink = "<a href = 'manage.php'>Manage Users</a>";
+    $managementLink = "<a href = 'manage.php'>Manage Users</a> ";
     $newUserLink = "<a href='new_user.php'>New User</a> ";
     $startDiv = "<div style='text-align:center'>";
     $endDiv = "</div>";
     switch ($_SERVER['REQUEST_URI']) {
         case "/manage.php":
-            printf("%s %s", $startDiv, "<strong>Manage Users</strong>");
+            printf("%s %s", $startDiv, "<strong>Manage Users</strong> ");
             printf("%s %s", $newUserLink, $endDiv);
             break;
         case "/new_user.php":
