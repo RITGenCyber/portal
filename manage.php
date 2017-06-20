86b35799a28a30f637a7efcbc3c6a61e2f05e3db
Joe Graham <jmg3484@rit.edu>
2017-06-20 09:58:38 -0400

    add user delete function

--- manage.php
+++ manage.php
@@ -1,12 +1,21 @@
+<?php
+    require_once "db.php";
+    $db = db_connect();
+    
+    if (!empty($_GET) && $_GET['action'] == "delete") {
+        $queryString = "DELETE FROM users WHERE id='" . $_GET['id'] . "'";
+        $query = mysqli_query($db, $queryString);
+        if (!$query) {
+            die("Error deleting user from database: " . mysqli_error($db));
+        }
+    }
+?>
+
 <html>
 <head>
 <title>Manage Users</title>
 <body>
 <?php
-    require_once "db.php";
-    
-    $db = db_connect();
-
     include "header.php";
     include "manage_header.php";
 
@@ -36,7 +45,11 @@
             echo "<td>" . $row['phoneNumber'] . "</td>";
             echo "<td>" . $row['admin'] . "</td>";
             echo "<td><a href=user_modify.php?id=" . $row['id'] . 
-                ">Edit User</a>";
+                ">Edit User</a></td>";
+            echo "<td><a onClick=\"javascript: " . 
+                "return confirm('Are you sure you want to delete this " . 
+                "user?');\" href=manage.php?action=delete&id=" . $row['id'] .
+                ">Delete User</a></td></tr>";
         }
         echo "</table></div>";
     }          
