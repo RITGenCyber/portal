b3d5c8cabf119658c219c481dec964096086aeb0
Joe Graham <jmg3484@rit.edu>
2017-06-20 15:38:07 -0400

    implement record delete

--- portal.php
+++ portal.php
@@ -9,6 +9,14 @@
 
     include "header.php";
 
+    if (isset($_GET) && !empty($_GET['id'])) {
+        $queryString = "DELETE FROM patients WHERE id='" . $_GET['id'] . "'";
+        $query = mysqli_query($db, $queryString);
+        if (!$query) {
+            die("Error deleting record from database: " . mysqli_error($db));
+        }
+    }
+
     $query = "SELECT * FROM patients";
     $results = mysqli_query($db, $query);
     if (!$results) {
@@ -19,7 +27,7 @@
         echo "<tr><th>Patient Number</th>";
         echo "<th>First Name</th>";
         echo "<th>Last Name</th>";
-        echo "<th>Gender</th>";
+        echo "<th>Sex</th>";
         echo "<th>Date of Birth</th>";
         echo "<th>Notes</th></tr>";
        
@@ -27,10 +35,14 @@
             echo "<tr><td>" . $row['id'] . "</td>";
             echo "<td>" . $row['firstName'] . "</td>";
             echo "<td>" . $row['lastName'] . "</td>";
-            echo "<td>" . $row['gender'] . "</td>";
+            echo "<td>" . $row['sex'] . "</td>";
             echo "<td>" . $row['birthDate'] . "</td>";
             echo "<td>" . $row['notes'] . "</td>";
             echo "<td><a href=modify.php?id=" . $row['id'] . ">Edit Record</a>";
+            echo "<td><a onClick=\"javascript: " . 
+                "return confirm('Are you sure you want to delete this " .
+                "record?');\" href=portal.php?action=delete&id=" . $row['id'] .
+                ">Delete Record</a>";
         }
         echo "</table></div>";
     }          
